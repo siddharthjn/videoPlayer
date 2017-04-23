df7957b220e0f2e7ee6f7d0fc2076667f3ea796a
Siddharth Jain <siddharth.jain@redspace.com>
2017-04-23 01:59:14 -0300

    Created video recorder class. Using Ecmascript 6 features

--- js/recorder.js
+++ js/recorder.js
@@ -5,6 +5,111 @@
 // MediaRecorder.stop -> stop recording (this will generate a blob of data)
 // URL.createObjectURL -> to create a URL from a blob, which we use as video src
 
+class videoRecorder{
+  startRecording() {
+    pauseButton.disabled = false;
+    if(streamingOn == 0){
+      videoRecorder.recordAgain();
+      document.getElementById('recordingSec').style.display ="none";
+    }
+    recorder = new MediaRecorder(stream);
+    recorder.addEventListener('dataavailable', videoRecorder.onRecordingReady);
+    recordButton.disabled = true;
+    stopButton.disabled = false;
+    recorder.start();
+  }
+
+  stopRecording() {
+    recordButton.disabled = false;
+    stopButton.disabled = true;
+    // Stopping the recorder will eventually trigger the 'dataavailable' event and we can complete the recording process
+    recorder.stop();
+    /*stop audio and video streaming*/
+    stream.getTracks()[0].stop();
+    stream.getTracks()[1].stop();
+    streamingOn = 0;
+    document.getElementById("live").style.display ="none";
+    recordButton.innerHTML ="record again";
+
+  }
+
+  static recordAgain(){
+    AdapterJS.webRTCReady(function(isUsingPlugin) {
+      if (typeof Promise === 'undefined') {
+        navigator.getUserMedia(constraints, onSuccess, onFailure);
+      } else {
+        navigator.mediaDevices.getUserMedia(constraints)
+          .then(onSuccess).catch(onFailure);
+      }
+    });
+
+    /*delaying execution by a second so that we get stream first */
+    setTimeout(function(){
+    recordButton.innerHTML = "recording";
+    document.getElementById("live").style.display ="block";
+    recorder = new MediaRecorder(stream);
+    recorder.addEventListener('dataavailable', videoRecorder.onRecordingReady);
+    recordButton.disabled = true;
+    stopButton.disabled = false;
+    recorder.start();}, 1000);
+  }
+
+  static onRecordingReady(e) {
+    document.getElementById('recordingSec').style.display = "block";
+    var videoRec = document.getElementById('recordingSec');
+    // e.data contains a blob representing the recording
+    videoRec.src = URL.createObjectURL(e.data);
+    //videoRec.play();
+  }
+
+  pauseRecording(){
+    if(recorder.state === "recording") {
+        recorder.pause();
+        pauseButton.innerHTML = "Resume";
+        // recording paused
+      } else if(recorder.state === "paused") {
+        recorder.resume();
+        pauseButton.innerHTML = "Pause"
+        // resume recording
+      }
+  }
+
+  localFileVideoPlayer() {
+    var URL = window.URL || window.webkitURL
+    var displayMessage = function (message, isError) {
+      var element = document.querySelector('#message')
+      element.innerHTML = message
+      element.className = isError ? 'error' : 'info'
+    }
+    var playSelectedFile = function (event) {
+      var file = this.files[0]
+      var type = file.type
+      var videoNode = document.getElementById('uploadedVid')
+      var canPlay = videoNode.canPlayType(type)
+      if (canPlay === '') canPlay = 'no'
+      var message = 'Can play type "' + type + '": ' + canPlay
+      var isError = canPlay === 'no'
+      displayMessage(message, isError)
+
+      if (isError) {
+        return
+      }
+
+      var fileURL = URL.createObjectURL(file)
+      videoNode.src = fileURL
+      document.getElementById('live').style.display ="none";
+      document.getElementById('recordingSec').style.display ="none";
+      document.getElementById('uploadedVid').style.display ="block";
+    }
+
+    var inputNode = document.querySelector('input')
+    inputNode.addEventListener('change', playSelectedFile, false)
+  }
+}
+
+
+var vr = new videoRecorder();
+
 /**Buttons are only for testing purpose */
 var recordButton, stopButton, recorder, liveStream;
 var liveVideo = document.getElementById('live');
@@ -14,13 +119,15 @@ pauseButton = document.getElementById('pause');
 pauseButton.disabled = true;
 recordButton.disabled = false;
 stopButton.disabled = false;
-
 /*Hiding recorded and uploaded video sction for now */
 document.getElementById('recordingSec').style.display ="none";
 document.getElementById('uploadedVid').style.display ="none";
-
 //counter to keep track if streaming has begun
 var streamingOn = 0;
+//button event listeners
+recordButton.addEventListener('click', vr.startRecording);
+stopButton.addEventListener('click', vr.stopRecording);
+pauseButton.addEventListener('click', vr.pauseRecording);
 
 var constraints = window.constraints = {
   audio: true,
@@ -52,8 +159,12 @@ var onFailure = function(error) {
   errorMsg('getUserMedia error: ' + error.name, error);
 };
 
-
-//
+function errorMsg(msg, error) {
+  errorElement.innerHTML += '<p>' + msg + '</p>';
+  if (typeof error !== 'undefined') {
+    console.error(error);
+  }
+}
 
 AdapterJS.webRTCReady(function(isUsingPlugin) {
   if (typeof Promise === 'undefined') {
@@ -64,122 +175,3 @@ AdapterJS.webRTCReady(function(isUsingPlugin) {
 
   }
 });
-
-
-recordButton.addEventListener('click', startRecording);
-stopButton.addEventListener('click', stopRecording);
-pauseButton.addEventListener('click', pauseRecording);
-
-
-      function startRecording() {
-        pauseButton.disabled = false;
-        if(streamingOn == 0){
-          recordAgain();
-          document.getElementById('recordingSec').style.display ="none";
-        }
-        recorder = new MediaRecorder(stream);
-        recorder.addEventListener('dataavailable', onRecordingReady);
-        recordButton.disabled = true;
-        stopButton.disabled = false;
-        recorder.start();
-      }
-
-      function stopRecording() {
-        recordButton.disabled = false;
-        stopButton.disabled = true;
-
-        // Stopping the recorder will eventually trigger the 'dataavailable' event and we can complete the recording process
-        recorder.stop();
-        /*stop audio and video streaming*/
-        stream.getTracks()[0].stop();
-        stream.getTracks()[1].stop();
-        streamingOn = 0;
-        document.getElementById("live").style.display ="none";
-        recordButton.innerHTML ="record again";
-        delete recorder;
-      }
-
-      function recordAgain(){
-
-        AdapterJS.webRTCReady(function(isUsingPlugin) {
-          if (typeof Promise === 'undefined') {
-            navigator.getUserMedia(constraints, onSuccess, onFailure);
-          } else {
-            navigator.mediaDevices.getUserMedia(constraints)
-              .then(onSuccess).catch(onFailure);
-
-          }
-        });
-
-        /*delaying execution by a second so that we get stream first */
-        setTimeout(function(){
-        recordButton.innerHTML = "recording";
-        document.getElementById("live").style.display ="block";
-        recorder = new MediaRecorder(stream);
-        recorder.addEventListener('dataavailable', onRecordingReady);
-        recordButton.disabled = true;
-        stopButton.disabled = false;
-        recorder.start();}, 1000);
-
-      }
-
-      function onRecordingReady(e) {
-        document.getElementById('recordingSec').style.display = "block";
-        var videoRec = document.getElementById('recordingSec');
-        // e.data contains a blob representing the recording
-        videoRec.src = URL.createObjectURL(e.data);
-        //videoRec.play();
-      }
-
-function errorMsg(msg, error) {
-  errorElement.innerHTML += '<p>' + msg + '</p>';
-  if (typeof error !== 'undefined') {
-    console.error(error);
-  }
-}
-
-function pauseRecording(){
-  if(recorder.state === "recording") {
-      recorder.pause();
-      pauseButton.innerHTML = "Resume";
-      // recording paused
-    } else if(recorder.state === "paused") {
-      recorder.resume();
-      pauseButton.innerHTML = "Pause"
-      // resume recording
-    }
-}
-
-
-(function localFileVideoPlayer() {
-
-  var URL = window.URL || window.webkitURL
-  var displayMessage = function (message, isError) {
-    var element = document.querySelector('#message')
-    element.innerHTML = message
-    element.className = isError ? 'error' : 'info'
-  }
-  var playSelectedFile = function (event) {
-    var file = this.files[0]
-    var type = file.type
-    var videoNode = document.getElementById('uploadedVid')
-    var canPlay = videoNode.canPlayType(type)
-    if (canPlay === '') canPlay = 'no'
-    var message = 'Can play type "' + type + '": ' + canPlay
-    var isError = canPlay === 'no'
-    displayMessage(message, isError)
-
-    if (isError) {
-      return
-    }
-
-    var fileURL = URL.createObjectURL(file)
-    videoNode.src = fileURL
-    document.getElementById('live').style.display ="none";
-    document.getElementById('recordingSec').style.display ="none";
-    document.getElementById('uploadedVid').style.display ="block";
-  }
-
-  var inputNode = document.querySelector('input')
-  inputNode.addEventListener('change', playSelectedFile, false)
-})()
